121b32d56d7eb7c6b928e743e2545b491c3f712d
Ashish <ashishmax31@gmail.com>
2018-10-01 19:05:51 +0530

    solution 2.59

---
+++ ex2-59.scm
@@ -0,0 +1,27 @@
+#lang scheme
+
+
+(define (intersection-set set1 set2)
+    (cond ((or (null? set1) (null? set2)) '())
+          ((element-of-set? set2 (car set1))
+                                            (cons (car set1)
+                                                  (intersection-set (cdr set1) set2)))
+          (else (intersection-set (cdr set1) set2))))
+
+
+(define (element-of-set? set item)
+    (cond ((null? set) false)
+          ((equal? (car set) item) true)
+          (else (element-of-set? (cdr set) item))))
+
+(define (adjoin-set set item)
+    (if (element-of-set? set item)
+        set
+        (cons item set)))
+
+(define (union-set set1 set2)
+    (cond ((null? set1) set2)
+          ((null? set2) set1)
+          ((not (element-of-set? set2 (car set1))) (cons (car set1)
+                                                    (union-set (cdr set1) set2)))
+          (else (union-set (cdr set1) set2))))
