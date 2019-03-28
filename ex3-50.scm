e5f0b9e1e83356bc91db4e3cbaf71eb8c57dc164
Ashish <ashishmax31@gmail.com>
2019-03-29 00:09:34 +0530

    Express cons stream and delay as macros

--- ex3-50.scm
+++ ex3-50.scm
@@ -1,3 +1,24 @@
+#lang scheme
+(define (force exp)
+    (exp))
+
+(define-syntax delay
+  (syntax-rules ()
+    ((_ exp) (lambda () exp))))
+
+(define-syntax cons-stream
+  (syntax-rules ()
+    ((_ a b) (cons a (delay b)))))
+
+(define (stream-car stream)
+    (car stream))
+
+(define (stream-null? stream) (null? stream))
+(define the-empty-stream '())
+
+(define (stream-cdr stream)
+    (force (cdr stream)))
+
 (define (stream-map proc . arg-streams)
     (if (stream-null? (car arg-streams))
         the-empty-stream
@@ -5,3 +26,14 @@
                       (apply stream-map 
                              (cons proc
                                    (map stream-cdr arg-streams))))))
+
+(define (enumerate-interval a b)
+    (if (> a b)
+        the-empty-stream
+        (cons-stream a
+                     (enumerate-interval (+ a 1) b))))
+
+(define (stream-ref stream n)
+    (if (= n 0)
+        (stream-car stream)
+        (stream-ref (stream-cdr stream) (- n 1))))
